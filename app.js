7f90500c81778940e3e82674cbab4419f8809f9d
Ahtsham <ahmadahtsham55@gmail.com>
2023-02-01 17:38:30 +0500

    new code to deploy site on cyclic.sh

--- app.js
+++ app.js
@@ -2705,15 +2705,22 @@ function verifyEmployee(req, res, next) {
 }
 
 // Serving the frontend
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       res.status(500).send(err);
+//     }
+//   );
+// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (_, res) => {
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 var port = process.env.PORT;
 if (port & process.env.IP) {
