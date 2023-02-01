59280c64a83e7b8ce645c1b6f6902d7fc19bdac4
Ahtsham <ahmadahtsham55@gmail.com>
2023-02-01 19:00:00 +0500

    update

--- app.js
+++ app.js
@@ -2706,10 +2706,10 @@ function verifyEmployee(req, res, next) {
 }
 
 // Serving the frontend
-app.use(express.static(path.join(__dirname, "*/client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "*/client/build/index.html"),
+    path.join(__dirname, "./client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
